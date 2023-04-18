7d292e1b4f8d94c4abcd880ae363188e3cbde35b
VSalzano <vincenzo.salzano@outlook.com>
2023-04-18 17:22:02 +0200

    raggiungiamo sezione domande

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,14 +1,33 @@
 let wpTemplate = document.getElementById('welcome-page').content;
 let wpHTML = document.importNode(wpTemplate, true);
-let content = document.getElementById('content')
- 
-window.addEventListener('load', () => {
-    content.appendChild(wpHTML);
-})
+let mainBox = document.getElementById('content')
+
+// aggiungo template welcome page
+
+mainBox.appendChild(wpHTML);
 
 let proceedBtn = document.getElementById('proceed')
 let check = document.getElementById ('check');
 
+let bpTemplate = document.getElementById('bp-template').content;
+let bpHTML = document.importNode(bpTemplate, true);
+
+// aggiungo template benchmark page
+
 proceedBtn.addEventListener('click', () => {
-    console.log(proceedBtn)
+    if(check.checked == true) {
+        mainBox.innerHTML = '';
+        mainBox.appendChild(bpHTML);
+    }
 })
+
+
+
+
+
+
+
+
+
+
+
