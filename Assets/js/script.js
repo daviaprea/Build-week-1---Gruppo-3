1c5bf540754944bef6b1febd86d0d6243eec70aa
VSalzano <vincenzo.salzano@outlook.com>
2023-04-19 10:09:46 +0200

    js domande

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -17,7 +17,36 @@ let bpHTML = document.importNode(bpTemplate, true);
 proceedBtn.addEventListener('click', () => {
     if(check.checked == true) {
         mainBox.innerHTML = '';
-        mainBox.appendChild(bpHTML);
+        fetch('https://opentdb.com/api.php?amount=10&category=18')
+        .then(res => res.json())
+        .then(res => {            
+            domande = res.results;
+            console.log(domande)
+            mainBox.appendChild(bpHTML);
+            
+            let question = document.querySelector('#question');
+            let answers = document.querySelector('.answers');
+            let optionsArr = [];
+
+            for(let q = 0 ; q < domande.length; q++){
+                optionsArr.push(domande[q].correct_answer);               
+            }
+
+            console.log(optionsArr);
+
+
+            let btn = [];
+            for (let i = 0; i < 4; i++) btn.push(document.createElement('button'));
+
+            domande.forEach(el => {
+                if(el.type == 'boolean') {
+
+                    answers.appendChild(btn[0]);
+                    answers.appendChild(btn[1]);
+                }
+            })
+        })
+        
     }
 })
 
