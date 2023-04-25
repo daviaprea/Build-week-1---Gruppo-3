aeafccfde27bd8adbce8267c6b29848112d17f6d
daviaprea <apredav@gmail.com>
2023-04-25 14:04:21 +0200

    Fine chart

--- Assets/js/script-prova2.js
+++ Assets/js/script-prova2.js
@@ -162,17 +162,85 @@ proceedBtn.addEventListener('click', () => {
                 document.getElementById("bp-timer-position").innerHTML="";
                 let rpTemp = document.getElementById('rp-template').content;
                 let rpHTML = document.importNode(rpTemp, true);
-                let correctScore = correct * 10;
-                let wrongScore = wrong * 10;
 
-                console.log(correct, wrong)
-
-                window.correctScore = correctScore;
-                window.wrongScore = wrongScore;
                 mainBox.appendChild(rpHTML);
 
-                document.querySelector('#c-percent span').innerHTML = `${correctScore}%`;
-                document.querySelector('#w-percent span').innerHTML = `${wrongScore}%`;
+                let ctx=document.getElementById("myChart").getContext("2d");
+
+                let chart=new Chart(ctx, {
+                    type: "doughnut",
+                    data: {
+                        labels: ["Wrong", "Correct"],
+
+                        datasets: [{
+                            label: "My first dataset",
+                            backgroundColor: ["#C2128D", "#00FFFF"],
+                            data: [w, c],
+                            hoverOffset: 10
+                        }]
+                    },
+                    options: {
+                        cutout: "70%",
+                        animation:{
+                            animateScale: true
+                        }
+                    },
+                    plugins: [{
+                        id: 'text',
+                        afterDatasetsDraw: function(chart)
+                        {
+                            let {ctx}=chart;
+                            ctx.save();
+                            let x=chart.getDatasetMeta(0).data[0].x;
+                            let y=chart.getDatasetMeta(0).data[0].y;
+                            let textTitle="";
+                            let textRes="";
+                            let textComm1=`We'll send you the certificate`;
+                            let textComm2=`in a few minutes.`;
+                            let textComm3=`Check your email (including`;
+                            let textComm4=`promotions / spam folder)`;
+
+                            if(c>w)
+                            {
+                                textTitle="Congratulations!";
+                                textRes="You passed the exam.";
+                            }
+
+                            else
+                            {
+                                textTitle="Oh no!";
+                                textRes="You didn't pass the exam.";
+                            }
+
+                            ctx.textAlign="center";
+                            ctx.textBaseline="middle";
+
+                            ctx.font="bold 18px Jost"
+                            ctx.fillStyle="white";
+                            ctx.fillText(textTitle, x, y-55);
+
+                            ctx.font="bold 18px Jost"
+                            ctx.fillStyle="#01EAEE";
+                            ctx.fillText(textRes, x, y-30);
+
+                            ctx.font="15px Jost"
+                            ctx.fillStyle="white";
+                            ctx.fillText(textComm1, x, y+10);
+                            ctx.font="15px Jost"
+                            ctx.fillStyle="white";
+                            ctx.fillText(textComm2, x, y+25);
+                            ctx.font="15px Jost"
+                            ctx.fillStyle="white";
+                            ctx.fillText(textComm3, x, y+40);
+                            ctx.font="15px Jost"
+                            ctx.fillStyle="white";
+                            ctx.fillText(textComm4, x, y+55);
+                        }
+                    }]
+                });
+
+                document.querySelector('#c-percent span').innerHTML = `${c}%`;
+                document.querySelector('#w-percent span').innerHTML = `${w}%`;
 
                 let cNum = document.getElementById('c-number');
                 let wNum = document.getElementById('w-number');
